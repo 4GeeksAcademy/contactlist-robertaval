4070bf91eea30db089b61fe4bffa6c9d56a85093
robertaval <roberta_valyte@hotmail.com>
2025-05-19 16:35:49 +0000

    finalizado

---
+++ src/services/apiServices.js
@@ -0,0 +1,62 @@
+export const getContacts = async (dispatch) => {
+    const response = await fetch('https://playground.4geeks.com/contact/agendas/robertaval/contacts')
+
+    if (!response.ok) {
+        await createAgenda(dispatch)
+        return
+    }
+
+    const data = await response.json();
+    dispatch({ type: 'SET_CONTACTS', payload: data.contacts });
+}
+
+export const createAgenda = async (dispatch) => {
+    const response = await fetch('https://playground.4geeks.com/contact/agendas/robertaval', { method: 'POST' })
+    getContacts(dispatch);
+}
+
+export const addContact = async (contact, dispatch, navigate) => {
+    console.log(contact)
+    const response = await fetch('https://playground.4geeks.com/contact/agendas/robertaval/contacts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: contact.name,
+            email: contact.email,
+            phone: contact.phone,
+            address: contact.address,
+        }
+        ),
+    });
+
+    await getContacts(dispatch);
+    navigate('/')
+};
+
+export const deleteContact = async (id, dispatch) => {
+    const response = await fetch(`https://playground.4geeks.com/contact/agendas/robertaval/contacts/${id}`, {
+        method: 'DELETE',
+    });
+
+    await getContacts(dispatch);
+};
+
+
+export const updateContact = async (id, formData, dispatch, navigate) => {
+
+    try {
+        const response = await fetch(`https://playground.4geeks.com/contact/agendas/robertaval/contacts/${id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(formData)
+    })  
+    
+    await getContacts(dispatch);
+        navigate('/')
+        // return getContacts("jacksama")
+    } catch (error) {
+
+        console.log(error)
+        
+}
+}
